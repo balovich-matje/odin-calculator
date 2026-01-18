9108d581d0179542f3fe0f8e85095152c2961a67
balovich-matje <balovichmatje@gmail.com>
2026-01-18 09:58:36 +0400

    Add 4 basic math functions and ph variables

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
